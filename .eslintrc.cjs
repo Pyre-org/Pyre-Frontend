38ba02c9aa46eee0fd784883ffad0fe33d1db6d5
chan-hwi <gazebo5@naver.com>
2024-01-18 00:36:45 +0900

    fix: eslint warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,18 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "@electron-toolkit/eslint-config-ts/recommended",
+    "@electron-toolkit/eslint-config-prettier",
+  ],
+  rules: {
+    semi: "error",
+    quotes: "double",
+    "prettier/prettier": {
+      error: {
+        endOfLine: "auto",
+      },
+    },
+  },
+};
