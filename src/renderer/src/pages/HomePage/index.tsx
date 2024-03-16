a5511a3538806c546be8a2acf226d5ec297528b3
chan-hwi <gazebo5@naver.com>
2024-03-17 00:51:02 +0900

    feat: add grid layout

--- src/renderer/src/pages/HomePage/index.tsx
+++ src/renderer/src/pages/HomePage/index.tsx
@@ -1,3 +1,4 @@
+import Loader from "@renderer/components/common/Loader";
 import {
   Avatar,
   AvatarFallback,
@@ -13,27 +14,35 @@ import {
 import { useGetRoom } from "@renderer/lib/queries/room";
 import { useGetSpace } from "@renderer/lib/queries/space";
 import { Feed } from "@renderer/types/schema";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, GridIcon, ListIcon } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const ViewMap = {
+  list: FeedListView,
+  grid: FeedGridView,
+};
+
 function HomePage() {
   const [mode, setMode] = useState<string>("others");
+  const [view, setView] = useState<string>("list");
 
   const {
     data: otherFeedsData,
     fetchNextPage: otherFetchNextPage,
     isFetchingNextPage: otherIsFetchingNextPage,
     hasNextPage: otherHasNextPage,
+    isLoading: otherIsLoading,
   } = useGetOthersFeedsInfinite({});
   const {
     data: myFeedsData,
     fetchNextPage: myFetchNextPage,
     isFetchingNextPage: myisFetchingNextPage,
     hasNextPage: myHasNextPage,
+    isLoading: myIsLoading,
   } = useGetMyFeedsInfinite({});
 
-  const otherFeeds =
+  const feeds =
     mode === "others"
       ? otherFeedsData?.pages.flatMap((page) => page.hits) ?? []
       : myFeedsData?.pages.flatMap((page) => page.hits) ?? [];
@@ -43,16 +52,33 @@ function HomePage() {
   const hasNextPage = mode === "others" ? otherHasNextPage : myHasNextPage;
   const fetchNextPage =
     mode === "others" ? otherFetchNextPage : myFetchNextPage;
+  const isLoading = mode === "others" ? otherIsLoading : myIsLoading;
+  const total =
+    mode === "others"
+      ? otherFeedsData?.pages?.[0]?.total ?? 0
+      : myFeedsData?.pages?.[0]?.total ?? 0;
 
   const handleFetchNext = () => {
     if (!isFetchingNextPage && hasNextPage) fetchNextPage();
   };
+
+  const ViewComp = ViewMap[view as keyof typeof ViewMap];
 
   return (
     <section className="flex-1 p-4 flex flex-col gap-4">
       <div className="flex items-center gap-4 mb-4">
         <h2 className="text-lg font-semibold">최신 피드</h2>
       </div>
+      <Tabs className="ml-auto" value={view} onValueChange={setView}>
+        <TabsList>
+          <TabsTrigger value="list">
+            <ListIcon className="size-4" />
+          </TabsTrigger>
+          <TabsTrigger value="grid">
+            <GridIcon className="size-4" />
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
       <Tabs value={mode} onValueChange={setMode} className="w-full">
         <TabsList className="w-full flex">
           <TabsTrigger value="others" className="flex-1">
@@ -63,11 +89,18 @@ function HomePage() {
           </TabsTrigger>
         </TabsList>
       </Tabs>
-      <div className="flex flex-col gap-4">
-        {otherFeeds.map((feed) => (
-          <FeedItem key={feed.id} feed={feed} />
-        ))}
-      </div>
+      {isLoading && (
+        <div className="py-16 mx-auto">
+          <Loader />
+        </div>
+      )}
+      {total > 0 ? (
+        <ViewComp feeds={feeds} />
+      ) : (
+        <div className="text-center text-muted-foreground text-sm py-16">
+          표시할 피드가 없습니다
+        </div>
+      )}
       {hasNextPage && (
         <Button
           variant="outline"
@@ -82,6 +115,26 @@ function HomePage() {
 }
 
 export default HomePage;
+
+function FeedListView({ feeds }: { feeds: Feed[] }) {
+  return (
+    <div className="flex flex-col gap-4">
+      {feeds.map((feed) => (
+        <FeedItem key={feed.id} feed={feed} />
+      ))}
+    </div>
+  );
+}
+
+function FeedGridView({ feeds }: { feeds: Feed[] }) {
+  return (
+    <div className="grid grid-cols-3 gap-2">
+      {feeds.map((feed) => (
+        <FeedGridItem key={feed.id} feed={feed} />
+      ))}
+    </div>
+  );
+}
 
 const FeedItem: React.FC<{ feed: Feed }> = ({ feed }) => {
   const { data: spaceData } = useGetSpace(feed.spaceId);
@@ -119,6 +172,36 @@ const FeedItem: React.FC<{ feed: Feed }> = ({ feed }) => {
         </div>
         <h2 className="font-semibold truncate">{feed.title}</h2>
         <img src={feed.imageUrl} alt="img" />
+      </CardHeader>
+      {feed.nickname && (
+        <CardContent>
+          <div className="flex gap-2">
+            <Avatar className="w-8 h-8">
+              <AvatarImage src={feed.profilePictureUrl} />
+              <AvatarFallback>
+                <span>{feed.nickname[0].toUpperCase()}</span>
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold">{feed.nickname}</span>
+              <span className="text-muted-foreground text-xs">{feed.cAt}</span>
+            </div>
+          </div>
+        </CardContent>
+      )}
+    </Card>
+  );
+};
+
+const FeedGridItem = ({ feed }: { feed: Feed }) => {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="flex gap-2 items-center flex-1">
+        <img
+          src={feed.imageUrl}
+          alt="img"
+          className="object-cover aspect-square"
+        />
       </CardHeader>
       {feed.nickname && (
         <CardContent>
